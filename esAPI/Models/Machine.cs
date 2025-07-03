15354f194afe10a50d478871e6ad52c9391fda3b
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-07-03 08:12:45 +0200

    Fixed indent issue
    Signed-off-by: Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>

--- esAPI/Models/Machine.cs
+++ esAPI/Models/Machine.cs
@@ -4,7 +4,7 @@ namespace esAPI.Models
 {
     public class Machine
     {
-                [Key]
+        [Key]
         public int MachineId { get; set; }
         public MachineStatus Status { get; set; }
         public float PurchasePrice { get; set; }
