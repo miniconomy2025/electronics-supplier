0759e3947057b77f8671972c63e8b6389cda05d0
Shailyn-Moodley-BBD <shailyn.moodley@bbd.co.za>
2025-07-03 09:26:58 +0200

    Model changes and Supplies API implementation (#10)

--- esAPI/Models/MaterialOrderItem.cs
+++ esAPI/Models/MaterialOrderItem.cs
@@ -1,15 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace esAPI.Models
 {
+    [Table("material_order_items")]
     public class MaterialOrderItem
     {
         [Key]
+        [Column("item_id")]
         public int ItemId { get; set; }
+        [Column("material_id")]
         public int MaterialId { get; set; }
+        [Column("amount")]
         public int Amount { get; set; }
+        [Column("order_id")]
         public int OrderId { get; set; }
+        [ForeignKey("MaterialId")]
         public Material? Material { get; set; }
+        [ForeignKey("OrderId")]
         public MaterialOrder? Order { get; set; }
     }
 } 
--- esAPI/Models/MaterialSupplier.cs
+++ esAPI/Models/MaterialSupplier.cs
@@ -1,11 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace esAPI.Models
 {
+    [Table("material_suppliers")]
     public class MaterialSupplier
     {
         [Key]
+        [Column("supplier_id")]
         public int SupplierId { get; set; }
+        [Column("supplier_name")]
         public string SupplierName { get; set; } = string.Empty;
     }
 } 
