cabc14e89fad8d6f1a3db461f89125d85392b151
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-07-04 14:52:15 +0200

    Fixed missing join issue on view
    Signed-off-by: Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>

--- esAPI/Migrations/V6__Views.sql
+++ esAPI/Migrations/V6__Views.sql
@@ -7,19 +7,33 @@ FROM materials m
 LEFT JOIN material_supplies s ON m.material_id = s.material_id AND s.processed_at IS NULL
 GROUP BY m.material_id, m.material_name;
 
+-- CREATE OR REPLACE VIEW available_electronics_stock AS
+-- SELECT
+--     COUNT(e.electronic_id) AS "availableStock",
+--     lv.electronics_price_per_unit AS "pricePerUnit"
+-- FROM electronics e
+-- INNER JOIN electronics_statuses es ON e.electronics_status = es.status_id
+-- CROSS JOIN (
+--     SELECT electronics_price_per_unit
+--     FROM lookup_values
+--     ORDER BY changed_at DESC
+--     LIMIT 1
+-- ) lv
+-- WHERE es.status = 'AVAILABLE';
+
 CREATE OR REPLACE VIEW available_electronics_stock AS
 SELECT
-    COUNT(e.electronic_id) AS "availableStock",
+    (SELECT COUNT(e.electronic_id)
+     FROM electronics e
+     INNER JOIN electronics_statuses es ON e.electronics_status = es.status_id
+     WHERE es.status = 'AVAILABLE') AS "availableStock",
     lv.electronics_price_per_unit AS "pricePerUnit"
-FROM electronics e
-INNER JOIN electronics_statuses es ON e.electronics_status = es.status_id
-CROSS JOIN (
+FROM (
     SELECT electronics_price_per_unit
     FROM lookup_values
     ORDER BY changed_at DESC
     LIMIT 1
-) lv
-WHERE es.status = 'AVAILABLE';
+) lv;
 
 CREATE OR REPLACE VIEW machine_status_counts AS
 SELECT
@@ -27,4 +41,4 @@ SELECT
     SUM(CASE WHEN ms.status = 'IN_USE' THEN 1 ELSE 0 END) AS "inUse",
     SUM(CASE WHEN ms.status = 'BROKEN' THEN 1 ELSE 0 END) AS "broken"
 FROM machines m
-JOIN machine_statuses ms ON m.machine_status
+INNER JOIN machine_statuses ms ON m.machine_status = ms.status_id;
