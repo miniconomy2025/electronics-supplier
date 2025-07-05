3f6d14e772784359752dd18de3e0800cd52c7109
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-07-05 13:33:33 +0200

    Getting rid of commented code

--- esAPI/Migrations/V6__Views.sql
+++ esAPI/Migrations/V6__Views.sql
@@ -7,20 +7,6 @@ FROM materials m
 LEFT JOIN material_supplies s ON m.material_id = s.material_id AND s.processed_at IS NULL
 GROUP BY m.material_id, m.material_name;
 
--- CREATE OR REPLACE VIEW available_electronics_stock AS
--- SELECT
---     COUNT(e.electronic_id) AS "availableStock",
---     lv.electronics_price_per_unit AS "pricePerUnit"
--- FROM electronics e
--- INNER JOIN electronics_statuses es ON e.electronics_status = es.status_id
--- CROSS JOIN (
---     SELECT electronics_price_per_unit
---     FROM lookup_values
---     ORDER BY changed_at DESC
---     LIMIT 1
--- ) lv
--- WHERE es.status = 'AVAILABLE';
-
 CREATE OR REPLACE VIEW available_electronics_stock AS
 SELECT
     (SELECT COUNT(e.electronic_id)
