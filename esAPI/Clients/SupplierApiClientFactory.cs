05913ba7fb908b98ea2c565ca75712a182de6461
Tevlen Naidoo <tevlen.naidoo@bbd.co.za>
2025-07-10 21:31:16 +0200

    Revert "Update inventory service (#52)"
    This reverts commit 43d32686ea23919123d8ce0ce9c92b6d5be22fab.

---
+++ esAPI/Clients/SupplierApiClientFactory.cs
@@ -0,0 +1,20 @@
+using esAPI.Interfaces;
+
+namespace esAPI.Clients;
+
+public class SupplierApiClientFactory(IServiceProvider serviceProvider)
+{
+    private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+    public ISupplierApiClient GetClient(string supplierName)
+    {
+        // Use the supplier's name to determine which client implementation to return.
+        // This is more robust than relying on hard-coded IDs.
+        return supplierName switch
+        {
+            "recycler" => _serviceProvider.GetRequiredService<SimulatedRecyclerApiClient>(),
+            "thoh" => _serviceProvider.GetRequiredService<SimulatedThohApiClient>(),
+            _ => throw new NotSupportedException($"No API client implementation found for supplier: {supplierName}")
+        };
+    }
+}
