8c50c086bf8fdc098c2fe42b179d1255edb2b4db
Tevlen-Naidoo-BBD <tevlen.naidoo@bbd.co.za>
2025-07-14 01:52:19 +0200

    Tired

--- esAPI/Models/BankBalanceSnapshot.cs
+++ esAPI/Models/BankBalanceSnapshot.cs
@@ -2,12 +2,17 @@ using System.ComponentModel.DataAnnotations;
 
 namespace esAPI.Models
 {
+    [Table("bank_balance_snapshots")]
     public class BankBalanceSnapshot
     {
         [Key]
+        [Column("id")]
         public int Id { get; set; }
+        [Column("simulation_day")]
         public int SimulationDay { get; set; }
+        [Column("balance")]
         public decimal Balance { get; set; }
+        [Column("timestamp")]
         public DateTime Timestamp { get; set; }
     }
 }
