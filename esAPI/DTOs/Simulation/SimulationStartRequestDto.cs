c2965f20087ae061a5d27275f913852fd15c23b0
Tevlen Naidoo <TNaidoo@ombank.co.za>
2025-10-21 18:53:08 +0200

    Made Epoch optional

--- esAPI/DTOs/Simulation/SimulationStartRequestDto.cs
+++ esAPI/DTOs/Simulation/SimulationStartRequestDto.cs
@@ -8,10 +8,10 @@ namespace esAPI.DTOs.Simulation
         /// <summary>
         /// UNIX epoch timestamp indicating when the actual simulation started
         /// This will be used to adjust internal time calculations
+        /// Optional - if not provided, current time will be used
         /// </summary>
         [JsonPropertyName("epochStartTime")]
-        [Required]
         [Range(0, long.MaxValue, ErrorMessage = "Epoch start time must be a positive number")]
-        public long EpochStartTime { get; set; }
+        public long? EpochStartTime { get; set; }
     }
 }
